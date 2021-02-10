5cdb7a4e11a35403ee7e18affa0bf18776b1d965
Changhwan Lee <chlee1252@gmail.com>
2021-02-10 10:42:35 +0900

    Initial Commit :tada:

---
+++ src/main/java/com/example/backendfinal/MainController.java
@@ -0,0 +1,15 @@
+package com.example.backendfinal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String main() {
+        return "<a href=\"https://google.com\">Google</a>";
+    }
+}
