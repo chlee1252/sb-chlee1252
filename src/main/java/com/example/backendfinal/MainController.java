e57a1464edc3a4e7735156bca360fb7b42f7f8ba
Changhwan Lee <chlee1252@gmail.com>
2021-02-10 14:44:01 +0900

    Dockerfile added

--- src/main/java/com/example/backendfinal/MainController.java
+++ src/main/java/com/example/backendfinal/MainController.java
@@ -2,6 +2,7 @@ package com.example.backendfinal;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,6 +11,12 @@ public class MainController {
     @GetMapping("/")
     @ResponseBody
     public String main() {
+        return "<a href=\"https://google.com\">Google</a>";
+    }
+
+    @PostMapping("/")
+    @ResponseBody
+    public String mainPost() {
         return "<a href=\"https://google.com\">Google</a>";
     }
 }
